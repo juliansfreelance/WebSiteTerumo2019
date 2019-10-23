0f8be6b43af323c3ac01627545e3766785d4c93c
Kevin Guacaneme López <nivek9810@gmail.com>
2019-10-23 17:24:30 -0500

    Avance de Aortic 23 de Octubre

--- src/app/pages/solutions/aortic/productos/sistemas-endovasculares-abdominales/soluciones-estandar/soluciones-estandar/soluciones-estandar.component.ts
+++ src/app/pages/solutions/aortic/productos/sistemas-endovasculares-abdominales/soluciones-estandar/soluciones-estandar/soluciones-estandar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as M from 'src/assets/js/materialize.min.js'
 
 @Component({
   selector: 'app-soluciones-estandar',
@@ -10,6 +11,7 @@ export class SolucionesEstandarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    M.Tabs.init(document.querySelectorAll('.tabs'), { swipeable: true })
   }
 
 }
