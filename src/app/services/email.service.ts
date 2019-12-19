bc73dbdb94dad5b3aff45ae5c40a6db0063311a3
Kevin Guacaneme López <nivek9810@gmail.com>
2019-12-18 22:36:23 -0500

    Lastest requeriments

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -9,6 +9,6 @@ export class EmailService {
   constructor(private _http: HttpClient) { }
 
   sendMessage(body){
-    return this._http.post('https://mail-node-terumo.herokuapp.com/formulario',body);
+    return this._http.post('https://murmuring-tundra-11198.herokuapp.com/formulario',body);
   }
 }
